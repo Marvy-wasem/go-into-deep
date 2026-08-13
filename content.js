71e2e315003bc1d5c5368423ee4c363761f37139
Marvy-wasem <marvysat2@gmail.com>
2026-08-13 14:53:19 +0300

    Update book link in content.js

--- content.js
+++ content.js
@@ -23,7 +23,7 @@ const siteData = {
     {
       title: "دعوة إلى حياة المسيح",
       description: "تدريبات للتشبه بشخصية يسوع المسيح — تأليف جان جونسون.",
-      link: "assets/books/daawa-ila-hayat-al-masih.pdf",
+      link: "assets/books/5_دعوة_الى_حياة_المسيح_christianlib_com.pdf",
       cover: "assets/books/book-cover.jpg"
     }
   ],
